4fdd1e611426b912eee913061380ebca24c09957
junhnno <withardor03@gmail.com>
2026-04-09 17:42:35 +0900

    Feat: 기본 퀴즈 데이터 5개 작성[game.py]

---
+++ game.py
@@ -0,0 +1,29 @@
+from quiz import Quiz
+
+DEFAULT_QUIZZES = [
+    Quiz(
+        question="준호의 키는?",
+        choices=["175cm", "177cm", "179cm", "181cm"],
+        answer=3
+    ),
+    Quiz(
+        question="준호가 가장 좋아하는 음식은?",
+        choices=["삼겹살", "족발", "치킨", "라면"],
+        answer=4
+    ),
+    Quiz(
+        question="준호의 폰 케이스 캐릭터는?",
+        choices=["라이언", "패트와 매트", "미니언즈", "스폰지밥"],
+        answer=2
+    ),
+    Quiz(
+        question="준호가 가장 잘하는 스포츠는?",
+        choices=["볼링", "농구", "당구", "탁구"],
+        answer=3
+    ),
+    Quiz(
+        question="준호가 나온 군대는?",
+        choices=["육군", "공군", "해군", "해병대"],
+        answer=4
+    ),
+]
